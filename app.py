2cd7656b67155e10ba47b0cb694272c3a3bc93fb
Carson Kirby <carson-kirby@uiowa.edu>
2024-04-17 11:39:26 -0500

    updates

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["SECRET_KEY"] = "seasdad(*2sffcra01^23sdet"
 CORS(app)
 
 # Get this URL from the Azure Overview page of your API web app
-api_url = "https://cdkirby-api-front-end.azurewebsites.net"  # base url for API endpoints
+api_url = "https://cdkirby-api.azurewebsites.net"  # base url for API endpoints
 
 
 
